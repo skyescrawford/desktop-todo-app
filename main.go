7c104ebb6d5eed0b4e2c851b8ed01ddc5e0a5854
Aziz <akashiseijurou414@gmail.com>
2026-01-18 23:24:23 +0700

    [feat] setup sqlite for persistence storage

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"embed"
+	"log"
 
+	_ "github.com/golang-migrate/migrate/v4/database/sqlite"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
+	_ "modernc.org/sqlite"
 )
 
 //go:embed all:frontend/dist
@@ -13,10 +18,36 @@ var assets embed.FS
 //go:embed build/appicon.png
 var icon []byte
 
+var DB *sql.DB
+
+func initDB() {
+	var err error
+	DB, err = sql.Open("sqlite", "./app.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt := `
+	CREATE TABLE IF NOT EXISTS todos (
+		id TEXT PRIMARY KEY,
+		title TEXT,
+		desc TEXT,
+		completed BOOLEAN,
+		createdAt TEXT DEFAULT CURRENT_TIMESTAMP,
+		completedAt TEXT
+	);
+	`
+	_, err = DB.Exec(stmt)
+	if err != nil {
+		log.Fatalf("Error creating table: %q: %s\n", err, stmt) // Log an error if table creation fails
+	}
+}
+
 func main() {
 	// Create an instance of the app structure
+	initDB()
 	app := NewApp()
-
+	repo := NewRepo()
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:            "todo app",
@@ -27,6 +58,7 @@ func main() {
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
+			repo,
 		},
 	})
 
